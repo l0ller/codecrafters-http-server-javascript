d10999b9f5742c2d81cbc0848c90c4a5e5a2a9d1
l0ller <uday98252@gmail.com>
2025-04-10 19:10:15 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -12,14 +12,14 @@ const server = net.createServer((socket) => {
     const [method,path,version] = requestline.split(" ");
     const headers = requestparts.slice(1);
     const headerObj = {};
-    console.log(method,path,version);
+   // console.log(method,path,version);
     
     headers.forEach(header =>{
         if(header){
             const[key,value] = header.split(": ");
             headerObj[key] = value;
-            console.log(key);
-            console.log(value);
+            // console.log(key);
+            // console.log(value);
         }
 
     });
@@ -31,6 +31,7 @@ const server = net.createServer((socket) => {
     else if(method === "GET" && path.startsWith("/echo/")){
         const slicedPath = path.substring("/echo/".length);
         response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" + slicedPath.length + "\r\n\r\n" + slicedPath;
+        console.log(response);
         socket.write(response);
     }
     else {
