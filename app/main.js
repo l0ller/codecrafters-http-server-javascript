ccb237f9475bb1e61a3d346e2369fc90068f3b9d
l0ller <uday98252@gmail.com>
2025-04-10 19:07:40 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -23,9 +23,14 @@ const server = net.createServer((socket) => {
         }
 
     });
-
+    let response;
     if(path !== "/"){
         response = "HTTP/1.1 404 Not Found\r\n\r\n";
+        socket.write(response);
+    }
+    else if(method === "GET" && path.startsWith("/echo/")){
+        const slicedPath = path.substring("/echo/".length);
+        response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" + slicedPath.length + "\r\n\r\n" + slicedPath;
         socket.write(response);
     }
     else {
