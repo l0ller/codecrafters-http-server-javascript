344cfec7d9156ce81c584e0af9b4d18dee4d6a2b
l0ller <uday98252@gmail.com>
2025-04-10 10:21:35 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -20,7 +20,7 @@ const server = net.createServer((socket) => {
 
     });
 
-    if(path !== ""){
+    if(path !== "/"){
         response = "HTTP/1.1 404 Not Found\r\n\r\n";
         socket.write(response);
     }
