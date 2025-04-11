228b7ca4b0dadcd61e953e2ad89906f6fb207a28
l0ller <uday98252@gmail.com>
2025-04-11 08:30:55 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -39,10 +39,12 @@ const server = net.createServer((socket) => {
                 response = "HTTP/1.1 404 Not Found\r\n\r\n";
                 console.log(response);
                 socket.write(response);
+                socket.end();
             }
             else{
                 response = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + data.length + "\r\n\r\n" + data;
                 socket.write(response);
+                socket.end();
             }
         });
 
@@ -55,12 +57,14 @@ const server = net.createServer((socket) => {
         response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" + slicedPath.length + "\r\n\r\n" + slicedPath;
         console.log(response);
         socket.write(response);
+        socket.end();
     }
 
     else if(method === "GET" && path.startsWith("/user-agent")){
         const userAgent = headerObj["User-Agent"];
         response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" + userAgent.length + "\r\n\r\n" + userAgent;
         socket.write(response);
+        socket.end();
     }
     
     
@@ -68,12 +72,14 @@ const server = net.createServer((socket) => {
         console.log(path);
         response = "HTTP/1.1 404 Not Found\r\n\r\n";
         socket.write(response);
+        socket.end();
     }
     else {
     const response = "HTTP/1.1 200 OK\r\n\r\n"
-    socket.write(response);}
+    socket.write(response);
+    socket.end();}
 
-    socket.end();
+    
 
   });
 });
